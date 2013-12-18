dcf3e9b702cf727fdeeb9ccf6735ebe382756331
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2013-12-18 10:14:49 +0100

    reuse the sysctl data collected on the first run

--- lib/facter/sensors.rb
+++ lib/facter/sensors.rb
@@ -1,5 +1,5 @@
-sensors = Facter::Util::Resolution.exec('/sbin/sysctl hw.sensors')
-sensors_list = sensors.scan(/^\S+/).collect { |s| s.gsub(/hw\.sensors\.(\w*)\..*/, '\1')}.uniq
+sensor_data = Facter::Util::Resolution.exec('/sbin/sysctl hw.sensors')
+sensors_list = sensor_data.scan(/^\S+/).collect { |s| s.gsub(/hw\.sensors\.(\w*)\..*/, '\1')}.uniq
 
 # List of all sensors in the system
 Facter.add('sensors') do
@@ -12,8 +12,7 @@ end
 # node below it. We assume that each sensor has the metrics one level deeper.
 # i.e., cpu0.temp0=44.00 degC
 sensors_list.each do |sensor|
-  sensor_metrics = Facter::Util::Resolution.exec("/sbin/sysctl hw.sensors.#{sensor}")
-  sensor_nodes = sensor_metrics.scan(/^\S+/).collect { |s| s.gsub(/hw\.sensors\.#{sensor}\.(\w*)=.*/, '\1')}.uniq
+  sensor_nodes = sensor_data.scan(/^\S+/).delete_if{|s| s !~ /#{sensor}/}.collect { |s| s.gsub(/hw\.sensors\.#{sensor}\.(\w*)=.*/, '\1')}.uniq
 
   sensor_nodes.each do |node|
     Facter.add("sensor_#{sensor}_#{node}") do
